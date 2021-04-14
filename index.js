6698cf9f20143774082b6a7bc27f1c4c056821a3
unknown <vandung030701@gmail.com>
2021-04-14 15:51:34 +0700

    Web_First

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function caculation() {
-    let fistNumber = document.getElementById('number-first').value;
-    let secondNumber = document.getElementById('number-second').value;
-    let caculation = document.getElementsByName('cal').value;
-    if (caculation == 'add') {
-        document.getElementsByName('result').value = fistNumber + secondNumber;
-    }
-    if (caculation == 'minus') {
-        document.getElementsByName('result').value = fistNumber - secondNumber;
-    }
-    if (caculation == 'multiply') {
-        document.getElementsByName('result').value = fistNumber * secondNumber;
-    }
-    if (caculation == 'divide') {
-        document.getElementsByName('result').value = fistNumber / secondNumber;
+    let fistNumber = Number(document.getElementById('number-first').value);
+    let secondNumber = Number(document.getElementById('number-second').value);
+    let caculation = document.getElementsByName('cal');
+    for (let i = 0; i < caculation.length; i++) {
+        if (caculation[i].checked) {
+            switch (caculation[i].value) {
+                case "add":
+                    document.getElementById('result').value = fistNumber + secondNumber;
+                    break;
+                case "minus":
+                    document.getElementById('result').value = fistNumber - secondNumber;
+                    break;
+                case "multiply":
+                    document.getElementById('result').value = fistNumber * secondNumber;
+                    break;
+                case "divide":
+                    document.getElementById('result').value = fistNumber / secondNumber;
+                    break;
+            }
+        }
     }
 }
